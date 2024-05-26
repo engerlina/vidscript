8d77b9696c5c729c1414f08ce2dc7db527208071
jonoc <38835253+engerlina@users.noreply.github.com>
2024-05-26 19:25:19 +1000

    Major changes v1.0 update
    Added:
    
    New Languages
    ejs framework
    Privacy and TOS
    SEO mods
    Logo

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 const { getSubtitles } = require('youtube-captions-scraper');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const session = require('express-session');
 const cron = require('node-cron');
 const { exec } = require('child_process');
+const { ClerkExpressWithAuth } = require('@clerk/clerk-sdk-node');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+
+// app.js
+const languageMap = require('./languageMap');
+
+console.log(languageMap);
+
+console.log('Clerk Publishable Key:', process.env.CLERK_PUBLISHABLE_KEY);
+console.log('Clerk Secret Key:', process.env.CLERK_SECRET_KEY);
+
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: '1f7f3c2d8e6c4b5a9d3c1e7f8b6a5d4csd433235gff',
+  secret: process.env.SESSION_SECRET || 'default-secret',
   resave: false,
   saveUninitialized: true
 }));
 
-const MAX_USES_PER_DAY = 100; // Set the maximum number of uses per day
-const TRANSCRIPTS_FOLDER = 'transcripts';
+const clerkMiddleware = ClerkExpressWithAuth({
+  publicKey: process.env.CLERK_PUBLISHABLE_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
-// Create the transcripts folder if it doesn't exist
+const MAX_USES_PER_DAY = 100;
+const TRANSCRIPTS_FOLDER = path.join(__dirname, 'transcripts');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 if (!fs.existsSync(TRANSCRIPTS_FOLDER)) {
   fs.mkdirSync(TRANSCRIPTS_FOLDER);
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.render('index', { url: req.protocol + '://' + req.get('host') + req.originalUrl });
+});
+
+app.get('/privacy-policy', (req, res) => {
+  res.render('privacy-policy', { url: req.protocol + '://' + req.get('host') + req.originalUrl });
+});
+
+app.get('/terms-of-service', (req, res) => {
+  res.render('terms-of-service', { url: req.protocol + '://' + req.get('host') + req.originalUrl });
+});
+
+app.get('/test', (req, res) => {
+  res.render('test', { url: req.protocol + '://' + req.get('host') + req.originalUrl });
 });
 
 const getYouTubeVideoId = (url) => {
@@ -37,39 +72,38 @@ const getYouTubeVideoId = (url) => {
   return matches ? matches[1] : null;
 };
 
-app.post('/transcribe', async (req, res) => {
-  const youtubeUrl = req.body.url;
-  const videoId = getYouTubeVideoId(youtubeUrl);
-
-  if (!videoId) {
-    console.error('Invalid video ID');
-    return res.status(400).json({ error: 'Invalid YouTube URL' });
-  }
-
-  // Check the user's usage count
-  if (!req.session.usageCount) {
-    req.session.usageCount = 0;
-  }
-
-  if (req.session.usageCount >= MAX_USES_PER_DAY) {
-    return res.status(429).json({ error: 'Daily usage limit reached' });
-  }
-
+const getAvailableLanguages = async (videoId) => {
   try {
-    const transcript = await getSubtitles({ videoID: videoId });
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/captions`, {
+      params: {
+        part: 'snippet',
+        videoId: videoId,
+        key: YOUTUBE_API_KEY
+      }
+    });
 
-    // Generate unique filenames using user ID and timestamp
-    const userID = req.session.id;
-    const timestamp = Date.now();
-    const txtFilename = `transcript_${userID}_${timestamp}.txt`;
-    const csvFilename = `transcript_${userID}_${timestamp}.csv`;
+    const captions = response.data.items;
+    const availableLanguages = captions.map(caption => {
+      const lang = caption.snippet.language;
+      return {
+        code: lang,
+        name: languageMap[lang] || lang
+      };
+    });
 
-    // Save the transcript to a TXT file
-    const txtFilePath = path.join(TRANSCRIPTS_FOLDER, txtFilename);
-    const transcriptText = transcript.map(entry => entry.text).join('\n');
-    fs.writeFileSync(txtFilePath, transcriptText);
+    return availableLanguages;
+  } catch (error) {
+    console.error(`An error occurred while fetching available languages: ${error.message}`);
+    return [];
+  }
+};
 
-    // Save the transcript to a CSV file
+const getTranscriptAndSave = async (videoId, selectedLanguage, identifier) => {
+  try {
+    const subtitles = await getSubtitles({ videoID: videoId, lang: selectedLanguage });
+    console.log('Subtitles:', subtitles);
+
+    const csvFilename = `transcript_${identifier}_${videoId}_${selectedLanguage}.csv`;
     const csvWriter = createCsvWriter({
       path: path.join(TRANSCRIPTS_FOLDER, csvFilename),
       header: [
@@ -78,31 +112,106 @@ app.post('/transcribe', async (req, res) => {
         { id: 'text', title: 'Text' }
       ]
     });
-    await csvWriter.writeRecords(transcript);
+    await csvWriter.writeRecords(subtitles);
 
-    // Increment the user's usage count
-    req.session.usageCount++;
+    const txtFilename = `transcript_${identifier}_${videoId}_${selectedLanguage}.txt`;
+    const txtFilePath = path.join(TRANSCRIPTS_FOLDER, txtFilename);
+    const subtitlesText = subtitles.map(entry => entry.text).join('\n');
+    fs.writeFileSync(txtFilePath, subtitlesText);
 
-    res.json({ transcript, txtFilename, csvFilename });
+    console.log(`Subtitles saved to ${csvFilename} and ${txtFilename}`);
+    return { txtFilename, csvFilename, subtitlesText };
   } catch (error) {
     console.error(`An error occurred: ${error.message}`);
-    res.status(500).json({ error: 'An error occurred while fetching the transcript.' });
+    throw error;
+  }
+};
+
+app.post('/transcribe', clerkMiddleware, async (req, res) => {
+  const youtubeUrl = req.body.url;
+  const videoId = getYouTubeVideoId(youtubeUrl);
+
+  if (!videoId) {
+    console.error('Invalid video ID');
+    return res.status(400).json({ error: 'Invalid YouTube URL' });
+  }
+
+  const isLoggedIn = !!req.auth.userId;
+  const identifier = isLoggedIn ? req.auth.userId : req.sessionID;
+
+  if (!isLoggedIn && req.session.usageCount >= MAX_USES_PER_DAY) {
+    return res.status(429).json({ error: 'Daily usage limit reached' });
+  }
+
+  try {
+    const availableLanguages = await getAvailableLanguages(videoId);
+    console.log('Available languages:', availableLanguages);
+
+    if (availableLanguages.length === 0) {
+      console.log(`No captions found for video: ${videoId}`);
+      return res.status(404).json({ error: 'No captions found for the video' });
+    }
+
+    if (availableLanguages.length === 1) {
+      const selectedLanguage = availableLanguages[0].code;
+      const { txtFilename, csvFilename, subtitlesText } = await getTranscriptAndSave(videoId, selectedLanguage, identifier);
+
+      if (!isLoggedIn) {
+        req.session.usageCount++;
+      }
+
+      res.json({
+        languageCodes: availableLanguages,
+        savedTranscripts: [{ languageCode: selectedLanguage, txtFilename, csvFilename, subtitlesText }]
+      });
+    } else {
+      res.json({ languageCodes: availableLanguages, savedTranscripts: [] });
+    }
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+    res.status(500).json({ error: 'An error occurred while fetching the subtitles.' });
   }
 });
 
-app.get('/download-txt/:filename', (req, res) => {
+app.get('/transcribe/:videoId/:lang', clerkMiddleware, async (req, res) => {
+  const videoId = req.params.videoId;
+  const selectedLanguage = req.params.lang;
+  const isLoggedIn = !!req.auth.userId;
+  const identifier = isLoggedIn ? req.auth.userId : req.sessionID;
+
+  try {
+    const { txtFilename, csvFilename, subtitlesText } = await getTranscriptAndSave(videoId, selectedLanguage, identifier);
+
+    if (!isLoggedIn) {
+      req.session.usageCount++;
+    }
+
+    res.json({
+      languageCodes: [{ code: selectedLanguage, name: getLanguageName(selectedLanguage) }],
+      savedTranscripts: [{ languageCode: selectedLanguage, txtFilename, csvFilename, subtitlesText }]
+    });
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+    res.status(500).json({ error: 'An error occurred while fetching the subtitles.' });
+  }
+});
+
+const getLanguageName = (code) => {
+  return languageMap[code] || code;
+};
+
+app.get('/download-txt/:filename', clerkMiddleware, (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(TRANSCRIPTS_FOLDER, filename);
   res.download(filePath, filename);
 });
 
-app.get('/download-csv/:filename', (req, res) => {
+app.get('/download-csv/:filename', clerkMiddleware, (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(TRANSCRIPTS_FOLDER, filename);
   res.download(filePath, filename);
 });
 
-// Scheduler: Run deleteOldTranscripts.js every hour
 cron.schedule('0 * * * *', () => {
   console.log('Running deleteOldTranscripts.js script');
 
@@ -119,6 +228,10 @@ cron.schedule('0 * * * *', () => {
   });
 });
 
+app.get('/modal', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'modal.html'));
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- public/app.js
+++ public/app.js
@@ -1,103 +1,282 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const urlInput = document.getElementById('urlInput');
-    const transcribeBtn = document.getElementById('transcribeBtn');
-    const results = document.getElementById('results');
-    const thumbnail = document.getElementById('thumbnail');
-    const transcription = document.getElementById('transcription');
-    const copyTranscriptionBtn = document.getElementById('copyTranscriptionBtn');
-    const downloadTxtBtn = document.getElementById('downloadTxtBtn');
-    const downloadCsvBtn = document.getElementById('downloadCsvBtn');
-  
-    const getYouTubeVideoId = (url) => {
-      const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-      const matches = url.match(regex);
-      console.log(`URL matches: ${matches}`);
-      return matches ? matches[1] : null;
-    };
-  
-    transcribeBtn.addEventListener('click', async () => {
-      const url = urlInput.value;
-      console.log(`URL input: ${url}`);
-      const videoId = getYouTubeVideoId(url);
-      console.log(`Extracted video ID: ${videoId}`);
-  
-      if (!videoId) {
-        alert('Invalid YouTube URL');
-        return;
-      }
-  
-      try {
-        const response = await fetch('/transcribe', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ url })
-        });
-        console.log(`Transcribe response status: ${response.status}`);
-        const data = await response.json();
+  const urlInput = document.getElementById('urlInput');
+  const transcribeBtn = document.getElementById('transcribeBtn');
+  const results = document.getElementById('results');
+  const thumbnail = document.getElementById('thumbnail');
+  const transcription = document.getElementById('transcription');
+  const copyTranscriptionBtn = document.getElementById('copyTranscriptionBtn');
+  const downloadTxtBtn = document.getElementById('downloadTxtBtn');
+  const downloadCsvBtn = document.getElementById('downloadCsvBtn');
+  const languageModal = document.getElementById('language-modal');
+  const languageOptionsContainer = document.getElementById('language-options');
+  const closeModalBtn = document.getElementById('close-modal-btn');
+  const buttonText = document.getElementById('buttonText');
+
+  function showModal() {
+    console.log('Showing modal');
+    languageModal.classList.add('show');
+  }
+
+  function hideModal() {
+    console.log('Hiding modal');
+    languageModal.classList.remove('show');
+  }
+
+  closeModalBtn.addEventListener('click', hideModal);
+
+  const getYouTubeVideoId = (url) => {
+    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const matches = url.match(regex);
+    console.log(`URL matches: ${matches}`);
+    return matches ? matches[1] : null;
+  };
+
+  function transcribeVideo() {
+    const url = urlInput.value;
+    console.log(`URL input: ${url}`);
+    const videoId = getYouTubeVideoId(url);
+    console.log(`Extracted video ID: ${videoId}`);
+
+    if (!videoId) {
+      alert('Invalid YouTube URL');
+      return;
+    }
+
+    // Change button to loading state
+    transcribeBtn.disabled = true;
+    buttonText.textContent = 'Loading...';
+    transcribeBtn.querySelector('svg').classList.add('hidden');
+    const spinner = document.createElement('span');
+    spinner.className = 'loading loading-spinner mr-2';
+    spinner.id = 'loadingSpinner';
+    transcribeBtn.insertBefore(spinner, buttonText);
+
+    fetch('/transcribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url })
+    })
+      .then(response => response.json())
+      .then(data => {
         console.log('Transcription data received:', data);
-  
+
         if (data.error) {
           console.error('Error from server:', data.error);
           alert('Error: ' + data.error);
           return;
         }
-  
+
+        if (!data.savedTranscripts) {
+          console.error('No saved transcripts data found');
+          alert('No saved transcripts data available');
+          return;
+        }
+
+        // Update the UI with the transcription data
         thumbnail.src = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-        transcription.textContent = data.transcript.map(entry => entry.text).join('\n');
         results.classList.remove('hidden');
-  
-        // Update the download buttons with the generated filenames
-        downloadTxtBtn.setAttribute('data-filename', data.txtFilename);
-        downloadCsvBtn.setAttribute('data-filename', data.csvFilename);
-      } catch (error) {
+
+        if (data.languageCodes.length > 1) {
+          console.log('Multiple languages available:', data.languageCodes);
+          languageOptionsContainer.innerHTML = '';
+
+          data.languageCodes.forEach(language => {
+            const button = document.createElement('button');
+            button.className = 'btn btn-neutral';
+            button.textContent = language.name;
+            button.addEventListener('click', () => handleLanguageSelection(videoId, language.code));
+            languageOptionsContainer.appendChild(button);
+          });
+
+          showModal();
+        } else {
+          console.log('Only one language available:', data.languageCodes);
+          // If only one language is available, display the transcription directly
+          const languageCode = data.languageCodes[0].code;
+          const savedTranscript = data.savedTranscripts.find(transcript => transcript.languageCode === languageCode);
+          if (savedTranscript) {
+            displayTranscription(savedTranscript);
+          } else {
+            console.error('No transcript found for the selected language');
+            alert('No transcript found for the selected language');
+          }
+        }
+      })
+      .catch(error => {
         console.error('An error occurred while fetching the transcription:', error);
         alert('An error occurred while fetching the transcription');
+      })
+      .finally(() => {
+        // Revert button to original state
+        transcribeBtn.disabled = false;
+        const loadingSpinner = document.getElementById('loadingSpinner');
+        if (loadingSpinner) {
+          loadingSpinner.remove();
+        }
+        buttonText.textContent = 'Transcribe';
+        transcribeBtn.querySelector('svg').classList.remove('hidden');
+      });
+  }
+
+  async function handleLanguageSelection(videoId, languageCode) {
+    console.log(`Language selected: ${languageCode}`);
+    try {
+      const response = await fetch(`/transcribe/${videoId}/${languageCode}`);
+      const transcriptData = await response.json();
+      console.log('Transcript data received for selected language:', transcriptData);
+
+      if (transcriptData.error) {
+        console.error('Error from server:', transcriptData.error);
+        alert('Error: ' + transcriptData.error);
+        return;
       }
-    });
-  
-    copyTranscriptionBtn.addEventListener('click', () => {
-      navigator.clipboard.writeText(transcription.textContent)
-        .then(() => {
-          alert('Transcription copied to clipboard!');
-        })
-        .catch((error) => {
-          console.error('Failed to copy transcription:', error);
+
+      const savedTranscript = transcriptData.savedTranscripts.find(transcript => transcript.languageCode === languageCode);
+      if (savedTranscript) {
+        displayTranscription(savedTranscript);
+        hideModal();
+      } else {
+        console.error('No transcript found for the selected language');
+        alert('No transcript found for the selected language');
+      }
+    } catch (error) {
+      console.error('An error occurred while fetching the transcription:', error);
+      alert('An error occurred while fetching the transcription');
+    }
+  }
+
+  function displayTranscription(savedTranscript) {
+    transcription.textContent = savedTranscript.subtitlesText;
+    downloadTxtBtn.setAttribute('data-filename', savedTranscript.txtFilename);
+    downloadCsvBtn.setAttribute('data-filename', savedTranscript.csvFilename);
+  }
+
+  transcribeBtn.addEventListener('click', transcribeVideo);
+
+  copyTranscriptionBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(transcription.textContent)
+      .then(() => {
+        alert('Transcription copied to clipboard!');
+      })
+      .catch((error) => {
+        console.error('Failed to copy transcription:', error);
+      });
+  });
+
+  downloadTxtBtn.addEventListener('click', async () => {
+    const filename = downloadTxtBtn.getAttribute('data-filename');
+    if (filename) {
+      try {
+        const response = await fetch(`/download-txt/${filename}`);
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        a.click();
+      } catch (error) {
+        console.error('An error occurred while downloading the TXT file:', error);
+      }
+    }
+  });
+
+  downloadCsvBtn.addEventListener('click', async () => {
+    const filename = downloadCsvBtn.getAttribute('data-filename');
+    if (filename) {
+      try {
+        const response = await fetch(`/download-csv/${filename}`);
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename;
+        a.click();
+      } catch (error) {
+        console.error('An error occurred while downloading the CSV file:', error);
+      }
+    }
+  });
+
+  // Clerk initialization
+  const frontendApi = 'pk_test_ZXhjaXRpbmctaHVza3ktOTYuY2xlcmsuYWNjb3VudHMuZGV2JA';
+
+  console.log('Loading Clerk with frontend API:', frontendApi);
+
+  const script = document.createElement('script');
+  script.setAttribute('data-clerk-publishable-key', frontendApi);
+  script.async = true;
+  script.src = 'https://cdn.jsdelivr.net/npm/@clerk/clerk-js@latest/dist/clerk.browser.js';
+  script.crossOrigin = 'anonymous';
+  script.addEventListener('load', () => {
+    console.log('Clerk loaded successfully');
+
+    const userButtons = document.getElementById('userButtons');
+    const userAvatar = document.getElementById('userAvatar');
+    const userAvatarImg = document.getElementById('userAvatarImg');
+    const signOutButton = document.getElementById('sign-out-button');
+    const profileLink = document.getElementById('profile-link');
+
+    if (profileLink) {
+      profileLink.addEventListener('click', () => {
+        window.Clerk.openUserProfile();
+      });
+    }
+
+    // Check if the user is signed in
+    window.Clerk.load({
+      publishableKey: frontendApi
+    }).then(() => {
+      console.log('Clerk components ready');
+
+      window.Clerk.addListener(({ user }) => {
+        if (user) {
+          // User is signed in
+          userButtons.style.display = 'none';
+          userAvatar.style.display = 'block';
+
+          // Set the user's profile image URL
+          userAvatarImg.src = user.imageUrl;
+
+          signOutButton.addEventListener('click', () => {
+            window.Clerk.signOut().then(() => {
+              location.reload();
+            });
+          });
+        } else {
+          // User is not signed in
+          userButtons.style.display = 'block';
+          userAvatar.style.display = 'none';
+        }
+      });
+
+      const signInButton = document.getElementById('sign-in-button');
+      const signUpButton = document.getElementById('sign-up-button');
+
+      console.log('Sign-in button:', signInButton);
+      console.log('Sign-up button:', signUpButton);
+
+      if (signInButton) {
+        signInButton.addEventListener('click', () => {
+          console.log('Sign-in button clicked');
+          window.Clerk.openSignIn();
         });
-    });
-  
-    downloadTxtBtn.addEventListener('click', async () => {
-      const filename = downloadTxtBtn.getAttribute('data-filename');
-      if (filename) {
-        try {
-          const response = await fetch(`/download-txt/${filename}`);
-          const blob = await response.blob();
-          const url = URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = filename;
-          a.click();
-        } catch (error) {
-          console.error('An error occurred while downloading the TXT file:', error);
-        }
+      } else {
+        console.warn('Sign-in button not found');
       }
-    });
-  
-    downloadCsvBtn.addEventListener('click', async () => {
-      const filename = downloadCsvBtn.getAttribute('data-filename');
-      if (filename) {
-        try {
-          const response = await fetch(`/download-csv/${filename}`);
-          const blob = await response.blob();
-          const url = URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = filename;
-          a.click();
-        } catch (error) {
-          console.error('An error occurred while downloading the CSV file:', error);
-        }
+
+      if (signUpButton) {
+        signUpButton.addEventListener('click', () => {
+          console.log('Sign-up button clicked');
+          window.Clerk.openSignUp();
+        });
+      } else {
+        console.warn('Sign-up button not found');
       }
+    }).catch((err) => {
+      console.error('Error loading Clerk components:', err);
     });
   });
+  document.head.appendChild(script);
+});
